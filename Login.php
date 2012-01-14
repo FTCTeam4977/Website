dd3c6bd2f6a3abfec1ff544e8ffafb9c6865f380
Ben <benjamin@thomasnetwork.net>
2012-01-14 11:01:50 -0500

    fixed blog login
    - both fields need text now
    - fixed link

--- Login.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Your User name is <?php echo $_POST["user"]; ?>!<br />
-Your password is <?php echo $_POST["password"]; ?>
-
-</body>
-</html>
--- form_action.php
+++ form_action.php
@@ -1,8 +1,48 @@
 <html>
+<head>
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>LAN Lords - FTC 4977</title>
+	<meta name="google-site-verification" content="HmLyK5rteHP_iEO6ujBozCudy67jNfxgcbbydDGrip4" />
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link href='http://fonts.googleapis.com/css?family=Niconne' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+</STYLE>
+</head>
 <body>
-
-Welcome <?php echo $_GET["fname"]; ?>!<br />
-You are <?php echo $_GET["lname"]; ?> years old.
-
-</body>
+<div class="cantainer">
+	<div calss="sixteen columns">
+		<center>
+		<h2>We</h2>
+			 <h4>If you got to this page by mistake, click <a href="http://www.team4977.org">here</a></h4>
+		<form action="Login.php" method="post">
+			Username: <input type= "text" name="user"><br>
+			Password: <input type= "password" name="password">
+			<input type="submit">
+		</form>
+		</center>
+	</div>	
+</div>	
+</body>	
 </html>
