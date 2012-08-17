a6df4b86182df6ff4fee026eb0209de72792c017
Ben <benjamin@thomasnetwork.net>
2012-08-17 13:48:36 -0400

    Media push

--- slideShow.js
+++ slideShow.js
@@ -1,4 +1,4 @@
-var Home = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
+var Home = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg"];
 var Robots = ["03.jpg","12.jpg"];
 var Pics = [];
 var place = 0;
