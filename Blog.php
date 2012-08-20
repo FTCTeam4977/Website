b1625beb53142fc4425e62df4fa059db6fa3bb71
Ben <benjamin@thomasnetwork.net>
2012-08-20 12:55:35 -0400

    starting more testing

---
+++ Blog.php
@@ -0,0 +1,8 @@
+<?php
+
+$data = explode(chr(29),$string);
+foreach($data as $info){
+	echo "\n test".$info;
+}
+
+?>
--- Login-Blog.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string = "Title".chr(29)."Author".chr(29)."Date".chr(29)."Content";
-$data = explode(chr(29),$string);
-foreach($data as $info){
-	echo "test\n".$info;
-}
-
-?>
