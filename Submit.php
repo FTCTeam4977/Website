26275482c2dd20410dd48508e8211d60f5fa8f11
Ben <benjamin@thomasnetwork.net>
2012-08-21 18:16:36 -0400

    submit testing

--- Submit.php
+++ Submit.php
@@ -3,7 +3,7 @@ $dir = "blog-files/";
 $NewFile = date("F j, Y, g:i a").".txt";
 $fileHandler = fopen($dir.$NewFile,"w");
 echo $fileHandler;
-$fileContent = "This is the title".chr(29)."By Benjamin Thomas".chr(29)."This is a post that I made";
+$fileContent = "This is the title".chr(29)."By Benjamin Thomas".chr(29)."This is another post that I made";
 echo "\n".$fileContent;
 fwrite($fileHandler,$fileContent);
 echo "writen";
