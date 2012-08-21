ac5bd68e2f5093e09cb2e7da3b490aef7f7d6596
Ben <benjamin@thomasnetwork.net>
2012-08-20 22:37:39 -0400

    more fixes

--- Blog.php
+++ Blog.php
@@ -5,7 +5,7 @@ foreach($dir as $file){
 	if($file != "." && $file != ".."){
 		$data = file_get_contents("blog-files/".$file);
 		$var = explode(";;;",$data);
-		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'>".$file."</div><div class='content'><p>".$var[2]."</p></div>";
+		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'>".substr($file, 0, -3)."</div><div class='content'><p>".$var[2]."</p></div>";
 	}
 }
 ?>
