89418fdfaec7bdb08957a58fec8bc018d9684d19
Ben <benjamin@thomasnetwork.net>
2012-08-20 22:54:41 -0400

    file check

--- Blog.php
+++ Blog.php
@@ -1,11 +1,14 @@
 <?php
+/*
 $dir = scandir("blog-files/");
 $dir = array_reverse($dir);
 foreach($dir as $file){
 	if($file != "." && $file != ".."){
 		$data = file_get_contents("blog-files/".$file);
 		$var = explode(";;;",$data);
-		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'>".substr($file, 0, -4)."</div><div class='content'><p>".$var[2]."</p></div>";
+		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'>".substr($file, 0, -4gg)."</div><div class='content'><p>".$var[2]."</p></div>";
 	}
 }
+*/
+echo scandir(".");
 ?>
