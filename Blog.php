f408f1c26d744e2ac267b0cd000747f0bfe53690
Ben <benjamin@thomasnetwork.net>
2012-08-20 21:14:41 -0400

    more testing for blog

--- Blog.php
+++ Blog.php
@@ -1,8 +1,11 @@
 <?php
-
-$data = explode(chr(29),$string);
-foreach($data as $info){
-	echo "\n test".$info;
-}
+$dir = scandir("blog-files/");
+foreach($dir as $file){
+	if($file != "." && $file != ".."){
+		$data = file_get_contents("blog-files/".$file);
+		$var = explode(";;;",$data);
+		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'>".$var[1]."</div>"
+	}
+}	
 
 ?>
