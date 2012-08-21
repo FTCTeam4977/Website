7ca91386812c46b4399cd4d0d01d958162c276d1
Ben <benjamin@thomasnetwork.net>
2012-08-21 08:11:54 -0400

    trail and error

--- Submit.php
+++ Submit.php
@@ -1,8 +1,11 @@
 <?php
 $dir = "blog-files/";
 $fileHandler = fopen($dir.$NewFile,"w");
+echo $fileHandler;
 $fileContent = "This is the title".chr(29)."By Benjamin Thomas".chr(29)."This is a post that I made";
+echo "\n".$fileContent;
 fwrite($fileHandler,$fileContent);
+echo "writen";
 fclose($fileHandler);
 echo "done";
 ?>
