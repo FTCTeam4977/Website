38ed364aa00c09177c94df84d2842676c8e16021
Ben <benjamin@thomasnetwork.net>
2012-08-21 18:08:23 -0400

    styling for blog

--- Blog.php
+++ Blog.php
@@ -5,7 +5,7 @@ foreach($dir as $file){
 	if($file != "." && $file != ".."){
 		$data = file_get_contents("blog-files/".$file);
 		$var = explode(chr(29),$data);
-		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'>".substr($file, 0, -4)."</div><div class='content'><p>".$var[2]."</p></div>";
+		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'><i>".substr($file, 0, -4)."</i></div><div class='content'><p>".$var[2]."</p></div>";
 	}
 }
 ?>
