86ffa13cb7d6428a5d7eee198c01a5272cd515bc
Ben <benjamin@thomasnetwork.net>
2012-08-21 08:10:23 -0400

    testing submit

--- Submit.php
+++ Submit.php
@@ -4,4 +4,5 @@ $fileHandler = fopen($dir.$NewFile,"w");
 $fileContent = "This is the title".chr(29)."By Benjamin Thomas".chr(29)."This is a post that I made";
 fwrite($fileHandler,$fileContent);
 fclose($fileHandler);
+echo "done";
 ?>
