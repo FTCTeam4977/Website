5e7d35d8c9dbf0ce4e564781af0a33369d880bf2
Ben <benjamin@thomasnetwork.net>
2012-08-21 08:03:18 -0400

    created submit file

---
+++ Submit.php
@@ -0,0 +1,7 @@
+<?php
+$dir = "blog-files/";
+$fileHandler = fopen($dir.$NewFile,"w");
+$fileContent = "This is the title".chr(29)."By Benjamin Thomas".chr(29)."This is a post that I made";
+fwrite($fileHandler,$fileContent);
+fclose($fileHandler);
+?>
