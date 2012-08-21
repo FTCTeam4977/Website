2058022a525f38000f2e0c081d22ddc5dca6dfcd
chuck-aka-ben <benjamin@thomasnetwork.net>
2012-08-21 18:17:59 -0300

    fixed (i think)

--- Blog.php
+++ Blog.php
@@ -1,12 +1,11 @@
 <?php
 $dir = scandir("blog-files/");
 $dir = array_reverse($dir);
-foreach($dir as $file){/*
+foreach($dir as $file){
 	if($file != "." && $file != ".."){
 		$data = file_get_contents("blog-files/".$file);
 		$var = explode(chr(29),$data);
 		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'>".substr($file, 0, -4)."</div><div class='content'><p>".$var[2]."</p></div>";
-	}*/
-	echo $file."the folder is up";
+	}
 }
 ?>
