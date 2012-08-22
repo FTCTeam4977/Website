a101e7753ce1ec3bb93a3bdabbb65ecb6e41419d
Ben <benjamin@thomasnetwork.net>
2012-08-21 20:16:14 -0400

    added user home page

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$_SESSION['isLoggedIn'] = true;
+$_SESSION['username'] = "Benjamin";
+$isLoggedIn = $_SESSION['isLoggedIn'];
+$username = $_SESSION['username'];
+if($isLoggedIn)
+	header("Location: Home.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<header>
+		<h2>Welcome <?echo $username;?>!</h2> 
+	</header>
+
+</body>
+</html>
