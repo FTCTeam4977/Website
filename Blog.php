d533202f4cc0b7f1000bec1ec37e81eb183154c7
Ben <benjamin@thomasnetwork.net>
2012-08-23 16:37:59 -0400

    reverted code

--- Blog.php
+++ Blog.php
@@ -10,10 +10,9 @@ $dir = scandir("blog-files/");
 $dir = array_reverse($dir);
 foreach($dir as $file){
 	if($file != "." && $file != ".."){
-		unlink($file);
-			// $data = file_get_contents("blog-files/".$file);
-			// $var = explode(chr(29),$data);
-			// echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'><i>".substr($file, 0, -4)."</i></div><div class='content'><p>".$var[2]."</p></div>";
+		$data = file_get_contents("blog-files/".$file);
+		$var = explode(chr(29),$data);
+		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'><i>".substr($file, 0, -4)."</i></div><div class='content'><p>".$var[2]."</p></div>";
 	}
 }
 ?>
