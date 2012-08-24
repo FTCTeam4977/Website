1bd9ffc587f5faff23ed948f1a51a7e92fc073ea
Ben <benjamin@thomasnetwork.net>
2012-08-24 08:26:41 -0400

    fix

--- Blog.php
+++ Blog.php
@@ -11,7 +11,7 @@ $dir = array_reverse($dir);
 foreach($dir as $file){
 	if($file != "." && $file != ".."){
 		echo $file." ";
-		unlink($file);
+		unlink("blog-files/".$file);
 		// $data = file_get_contents("blog-files/".$file);
 		// 		$var = explode(chr(29),$data);
 		// 		echo "<div class='title'><h2>".$var[0]."</h2></div><div class='author'><p>".$var[1]."</p></div><div class='date'><i>".substr($file, 0, -4)."</i></div><div class='content'><p>".$var[2]."</p></div>";
