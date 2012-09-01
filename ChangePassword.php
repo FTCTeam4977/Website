8f5922e8bf2ebace839048d6dc75b0d228b34119
Ben <benjamin@thomasnetwork.net>
2012-09-01 11:14:45 -0400

    debug

--- ChangePassword.php
+++ ChangePassword.php
@@ -15,6 +15,7 @@ $dbFile = file_get_contents("users.db");
 $rows = explode("\n",$dbFile);
 foreach($rows as $row){
 	$data = explode(",",$row);
+	echo $data[2];
 	if($data[2] == $user){
 		echo "The user is ".$data[2].", and the password is ".$data[1];
 	}
