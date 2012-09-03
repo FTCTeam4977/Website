0308731c9bb34ec7d9c62409fc79cccf23c8a490
Andrew <andrew@lobos.me>
2012-09-03 10:39:54 -0400

    Added an improved database system

--- ChangePassword.php
+++ ChangePassword.php
@@ -1,26 +1,11 @@
 <?php
 include "LoginCheck.php";
+include "Database.php";
+
 if(!$_POST["NewPasswd"])
-die();
-$newPassword = $_POST["NewPasswd"];
-$starttime = time();
-while(file_exists("db.lock"))
-{
-	$timepassed = time()-$starttime;
-	if ( $timepassed>5)
-	{
-		unlink("db.lock");
-		break;
-	}
-}
-touch("db.lock");
-$dbFile = file_get_contents("users.db");
-$rows = explode("\n",$dbFile);
-foreach($rows as $row){
-	$data = explode(",",$row);
-	if($data[2] == $user){
-		$data[1] = $newPassword;
-	}
-}
-unlink("db.lock");
+	die();
+	
+$db = new Database();
+$db->changePassword($_SESSION['user'], $_POST["NewPasswd"]);
+	
 ?>
--- validate.php
+++ validate.php
@@ -1,29 +1,9 @@
 <?php
 session_start();
+include "Database.php";
+
 $username = $_POST["user"];
 $password = $_POST["passwd"];
-class Database
-{	
-	function __construct()
-	{	
-		$this->users = array();
-		touch("users.db");
-		$data = file_get_contents("users.db");
-		$lines = explode("\n", $data);	
-		foreach ( $lines as $line )
-		{
-			$user = explode(",", $line);
-			$this->users[$user[0]] = $user; 
-		}
-	}
-
-	function check($username, $password)
-	{
-		if ( !array_key_exists($username, $this->users) )
-			return false;
-		return $password==$this->users[$username][1];
-	}
-}
 
 $db = new Database();
 if(!$db->check($username, $password))
