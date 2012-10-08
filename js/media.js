ad17d9f5410b0e3307e7bccd7ac9d9738951dac9
Ben <benjamin@thomasnetwork.net>
2012-10-08 16:55:09 -0400

    added Path jump to media + loading
    - path is now a group of  clickable links
    -Loading gif

--- js/media.js
+++ js/media.js
@@ -1,4 +1,5 @@
-CurrentDir = "./";
+var CurrentDir = "./";
+var DisplayPath = "";
 function GetDirContent(dir){
 	$.ajax({
 			url:"media/getDir.php",
@@ -10,13 +11,31 @@ function GetDirContent(dir){
 			});
 }
 
-function Browser(content){
-	var data = content.split(String.fromCharCode(29));
-	CurrentDir = data[0];
-	DisplayDir = CurrentDir.split("/media");
-	$("#Browser").html("/media" + DisplayDir[1] +" " + ClickLink("GetDirContent('../')", "Go back") + "<br>" + data[1]);
+function LinkContent(dir){
+	$.ajax({
+			url:"media/getDir.php",
+				type:"post",
+				data:"dir="+dir,
+				success:function (result){
+					Browser(result);
+				}
+			});
 }
 
 function ClickLink(link, inner){
 	return "<a onclick=\"" + link + "\">" + inner + "</a>";
+}
+
+function Browser(content){
+	var data = content.split(String.fromCharCode(29));
+	var Path = "";
+	DisplayPath = "";
+	CurrentDir = data[0];
+	DisplayDir = CurrentDir.split("/media");
+	var Folders = DisplayDir[1].split("/");
+	for(i=1; i<Folders.length;i++){
+		Path = Path + Folders[i] + "/";
+		DisplayPath = DisplayPath + ClickLink("LinkContent('" + Path + "')", Folders[i] + "/");
+	}
+	$("#Browser").html( ClickLink("LinkContent('.')","media//:") + DisplayPath +" " + ClickLink("GetDirContent('..')", "Go back") + "<br>" + data[1]);
 }
