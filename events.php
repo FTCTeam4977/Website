f1bac4195ba7606805ac35327baffb2128ef8554
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-09-08 20:10:24 -0400

    All pages successfully checked as HTML5, enabled links, added upcoming events

--- events.php
+++ events.php
@@ -14,7 +14,7 @@ include_once("include.php");
 		<div class="navbar navbar-inverse navbar-fixed-top">
 			<div class="navbar-inner">
 				<div class="container">
-					<a type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
